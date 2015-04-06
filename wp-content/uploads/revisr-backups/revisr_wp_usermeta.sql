7691455c12bdff2537992a2a2b85714eac4caa44
wp-admin <root@web001b.wd>
2015-04-06 21:43:22 +1200

    Daily backup - April 6, 2015

---
+++ wp-content/uploads/revisr-backups/revisr_wp_usermeta.sql
@@ -0,0 +1,2 @@
+Warning: Using a password on the command line interface can be insecure.
+mysqldump: Got error: 2005: Unknown MySQL server host 'localhost:/var/run/mysqld/mysqld.sock' (0) when trying to connect
